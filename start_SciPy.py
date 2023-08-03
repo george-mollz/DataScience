e20b9a9ae482a45074fb1e6dd9163acdcfdc04f8
george-mollz <gmollel74@gmail.com>
2023-08-03 13:44:08 +0300

    modified

--- start_SciPy.py
+++ start_SciPy.py
@@ -1,4 +1,13 @@
+#import scipy 
+
 from scipy import constants
 
-print(constants.liter
-)
+print(constants.liter)
+
+"""
+print("PRINTING SCIPY VERSION")
+print(scipy.__version__)
+"""
+
+print("\n")
+print(constants.pi)
