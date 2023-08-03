d9db130f68be8adc66679e9544e5be4e751698d9
george-mollz <gmollel74@gmail.com>
2023-08-03 13:25:25 +0300

    8/3/2023

--- startPandas.py
+++ startPandas.py
@@ -4,7 +4,7 @@ mydataset = {
     'cars' : ["BMW", "Volvo", "Ford"],
     'passings' : [3,7,2]
 }
-myvar = pd.DataFrame(mydataset)
+myvar = pd.DataFrame(mydataset)#Datasets to be discussed later...
 print(myvar)
 
 print("\n")
@@ -51,6 +51,33 @@ print("y")
 print("\n")
 print("Key/Value Ojects as Series")
 #Key/Value Ojects as Series
-calories = {"day":420, "day2": 380, "day3": 390}
+calories = {"day1":420, "day2": 380, "day3": 390}
 myvar = pd.Series(calories)
 print(myvar)
+
+print("\n")
+print("SELECTING ONLY SOME OF THE ITEMS IN THE DICTIONARY ")
+#SELECTING ONLY SOME OF THE ITEMS IN THE DICTIONARY
+myvar  = pd.Series(calories, index=["day1","day2"])
+print(myvar)
+
+
+
+
+
+
+print('\n')
+print("\n")
+print("DATA FRAMES")
+#DATA FRAMES
+data = {
+    "calories": [420,380,390],
+    "duration": [50,40,45]
+}
+#load data into dataFrame object:
+myvar = pd.DataFrame(data)#contains two series: calories and duration
+print(myvar)
+
+print("\n")
+print(" ")
+#
