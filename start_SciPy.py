8e351682b66f4892a0de098b55c008f22f132f6e
george-mollz <gmollel74@gmail.com>
2023-08-03 13:48:09 +0300

    modified

--- start_SciPy.py
+++ start_SciPy.py
@@ -1,13 +1,16 @@
-#import scipy 
+
 
 from scipy import constants
 
 print(constants.liter)
 
 """
+#import scipy 
 print("PRINTING SCIPY VERSION")
 print(scipy.__version__)
 """
 
 print("\n")
+print("OBTAIN VALUE OF CONSTANT 'PI'")
 print(constants.pi)
+
